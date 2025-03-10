76fb966c94ddf1db740901779b6c4751d750a2ad
Guri536 <102018637+Guri536@users.noreply.github.com>
2025-03-10 20:05:28 +0530

    Trying

--- test/resources/views/welcome.blade.php
+++ test/resources/views/welcome.blade.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <x-includes.includes-top />
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="display-1">Tie</div>
-    Ello there {{$user}}
-    @if (TRUE)
-    <br>WowZeros
-    @endif
-    <br>
-    <x-alerts.alert_main :message="'This is bad!'" style="border: 9px solid black;">
-        What de hell;
-    </x-alerts.alert_main>
-
-<x-includes.includes-bottom />
-</body>
-</html>
+<x-layout topLinks>
+    Hey there!!
+</x-layout>
